16005557feb9fa1910f60fcbc7eea195c3c0bae4
Victor <adsvictor@outlook.com>
2018-11-05 16:46:09 -0200

    Banco de dados MySQL e criação automática de tabelas

--- src/main/java/com/blog/webedia/domain/Autor.java
+++ src/main/java/com/blog/webedia/domain/Autor.java
@@ -1,11 +1,20 @@
 package com.blog.webedia.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import java.io.Serializable;
 
+@Entity
 public class Autor implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
 	private String nome;
 
 	public Autor() {
