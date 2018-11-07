589e3a026f4fe57b825fc7c960b259256a860567
Victor <adsvictor@outlook.com>
2018-11-07 17:25:05 -0200

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/blog/webedia/resources/AutorResource.java
+++ src/main/java/com/blog/webedia/resources/AutorResource.java
@@ -4,11 +4,13 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -54,4 +56,14 @@ public class AutorResource {
 		List<Autor> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
+
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<Autor>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+		Page<Autor> list = service.findPage(page, linesPerPage, orderBy, direction);
+		return ResponseEntity.ok().body(list);
+	}
+
 }
--- src/main/java/com/blog/webedia/services/AutorService.java
+++ src/main/java/com/blog/webedia/services/AutorService.java
@@ -3,6 +3,9 @@ package com.blog.webedia.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.blog.webedia.domain.Autor;
@@ -40,5 +43,10 @@ public class AutorService {
 
 	public List<Autor> findAll() {
 		return repo.findAll();
+	}
+
+	public Page<Autor> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
